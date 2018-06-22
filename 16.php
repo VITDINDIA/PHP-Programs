638d2fe00991bd284b6894bc47095f1bcacd13e1
“Abhinav” <vdc@vidya.edu.in>
2018-06-22 15:17:43 +0530

    HTML Programs

---
+++ 16.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="content-type" content="e-learning" />
+<meta name="keywords"  content="php, asp, jsp, tutorial" />
+<title>e learning</title>
+</head>
+
+<body  bgcolor="#FFFF00">
+<h1> Heading </h1>
+<h2> Heading </h2><h3> Heading </h3><h4> Heading </h4>
+<h5> Heading </h5><h6> Heading </h6>
+
+<hr size="20px;" color="#FF0000" />
+
+<p style="text-align:center;">
+<strong>Abhinav,</strong> anjknaskjndkjnaskjdn 
+<em>lkaknskjndkjnkajnskjn</em> <br/> jnakjsdnkjnas kjnaskjd
+asmdlkmklamsldml
+kanslkdklmaslkmdlsad </p>
+
+<br><br>
+
+<p><font size="+5" >aslmdlkmasdamsnlkdlkas </font></p>
+
+<h1>Courses Available </h1>
+<ol type="A">
+<li> Java </li><li> PHP </li> <li> ASP </li>
+</ol>
+<h1>Courses Available </h1>
+<ul type="square">
+<li> Java </li><li> PHP </li> <li> ASP </li>
+</ul>
+<img src="1.jpg" height="300px;" width="100px;" border="12" />
+<a href="17.php">Click Here</a>
+
+
+
+
+</body>
+</html>
---
+++ 18.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<table border="1" width="100%" bgcolor="#FFFF00">
+<thead> <th>Srno</th> <th>Name</th> <th>Roll Number </th>
+<th> Course </th><th>Contact </th> </thead>
+<tbody>
+<tr>
+<td> 1</td><td> Abhinav</td><td> 90812121</td><td> Btech</td>
+<td>980815644</td>
+</tr>
+<tr>
+<td> 2</td><td> Abhinav</td><td> 90812121</td><td> Btech</td>
+<td>980815644</td>
+</tr>
+<tr>
+<td> 3</td><td> Abhinav</td><td colspan="2"> 90812121 Btech</td>
+<td>980815644</td>
+</tr>
+</tbody>
+</table>
+</body>
+</html>
